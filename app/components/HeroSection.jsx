f7b4274b77419c9cac16eedf4a5c45eebaa609eb
Kevin06x <kevinzhang06x@gmail.com>
2024-05-06 15:25:07 -0700

    Typing Text Fix

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -1,9 +1,14 @@
 "use client";
-import React from 'react';
+import React, { useState } from 'react';
 import Image from "next/image";
 import { TypeAnimation } from 'react-type-animation'
 
 const HeroSection = () => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleHover = () => {
+    setIsHovered(!isHovered);
+  };
   return (
     <section className="container mx-auto px-4 lg:px-0">
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-4'>
@@ -11,22 +16,22 @@ const HeroSection = () => {
           <h1 className="text-white mb-4 text-3xl sm:text-5xl  lg:text-6xl font-extrabold ">
                 <span className='text-transparent bg-clip-text bg-gradient-to-r from-zinc-200 via-zinc-300 to-zinc-400' style={{fontFamily:'Rowdies, sans-serif', fontWeight: 600, fontStyle: 'normal'}}>Hello, I&apos;m Kevin,{" "}</span>
                 <br></br>
-                <TypeAnimation
-                    sequence={[
-                        "a Data Analyst",
-                        1000,
-                        "a Computer Science Student",
-                        1000,
-                        "a Software Engineer",
-                        1000,
-                        "an Avid Programmer",
-                        1000,
-                    ]}
-                    wrapper="span"
-                    speed={50}
-                    repeat={Infinity}
-                    style={{fontFamily:'Rowdies, sans-serif', fontWeight: 600, fontStyle: 'normal'}}
-                />
+                <span className="inline-block h-[100px] lg:h-[125px]">
+                  <TypeAnimation
+                      sequence={[
+                          "a Computer Science Student",
+                          1000,
+                          "an Aspiring Software Engineer",
+                          1000,
+                          "an Avid & Productive Programmer",
+                          1000,
+                      ]}
+                      wrapper="span"
+                      speed={50}
+                      repeat={Infinity}
+                      style={{fontFamily:'Rowdies, sans-serif', fontWeight: 600, fontStyle: 'normal'}}
+                  />
+                </span>
             </h1>
           <p className='text-[#ADB7BE] sm:text-lg text-lg mb-6 lg:text-xl' style={{fontFamily: 'Source Code Pro, monospace', fontWeight: 600, fontStyle: 'normal'}}>
            Welcome to my Portfolio Website! Here you can see all of my Projects, Experiences, and Skills that I&apos;ve built as a Programmer. However, if you want to know about me more as a person, then there&apos;s that too! 
@@ -34,14 +39,22 @@ const HeroSection = () => {
           </p>
 <div>
           <a href="https://github.com/Kevin06x" target="_blank">
-            <button className='px-1 py-1 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-400 via-blue-600 to-blue-800 hover:bg-slate-100 text-white mt-3'>
-                <span className='block bg-[#121212] hover:bg-slate-800 rounded-full px-10 py-2'>
+            <button className={`px-1 py-1 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-400 via-blue-600 to-blue-800 hover:bg-slate-100 text-white mt-3 transition-transform duration-300 transform hover:scale-105 ${
+          isHovered ? 'shadow-lg' : ''
+        }`}
+        onMouseEnter={handleHover}
+        onMouseLeave={handleHover}>
+                <span className='block bg-[#121212] rounded-full px-10 py-2'>
                   <strong className='text-lg' style={{fontFamily: 'Source Code Pro, monospace', fontWeight: 600, fontStyle: 'normal'}}>Github</strong></span>
             </button>
             </a>
             <a href="https://www.linkedin.com/in/kevin06x/" target='_blank'>
-              <button className='px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-400 via-blue-600 to-blue-800 hover:bg-slate-100 text-white mt-3'>
-                  <span className='block bg-[#121212] hover:bg-slate-800 rounded-full px-10 py-2'>
+              <button className={`px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-400 via-blue-600 to-blue-800 hover:bg-slate-100 text-white mt-3 transition-transform duration-300 transform hover:scale-105 ${
+          isHovered ? 'shadow-lg' : ''
+        }`}
+        onMouseEnter={handleHover}
+        onMouseLeave={handleHover}>
+                  <span className='block bg-[#121212] rounded-full px-10 py-2'>
                     <strong className='text-lg' style={{fontFamily: 'Source Code Pro, monospace', fontWeight: 600, fontStyle: 'normal'}}>LinkedIn</strong></span>
               </button>
             </a>
