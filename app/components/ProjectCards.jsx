4060d1429b069c170c046c56506c6cea2b62f1b5
Kevin Zhang <KevinZhang06x@gmail.com>
2024-05-07 23:11:43 -0700

    Image Height Fix

--- app/components/ProjectCards.jsx
+++ app/components/ProjectCards.jsx
@@ -91,7 +91,7 @@ const ProjectCards = () => {
           {ProjectsData.map(({ id, image, title, description, demo, repo }) => {
             return (
               <div key={id} className='text-white shadow-md rounded-lg overflow-hidden relative group'>
-                <img src={image} alt={title} className='w-full max-w-[400px] h-[300px] rounded-lg' />
+                <img src={image} alt={title} className='w-full max-w-[500px] lg:max-h-[250px] rounded-lg max-h-[300px]' />
                 <div className='absolute left-0 top-[-100%] text-white opacity-0 group-hover:opacity-100 group-hover:top-[0] p-4 w-full h-full bg-black/60 backdrop-blur-sm duration-500'>
                   <div className='space-y-4'>
                     <Slide cascade>
